71df4cbc089d19984408dfb26df065e6e42023d4
Robson Lázaro <97258787+RobsonLazaro@users.noreply.github.com>
2023-03-23 16:16:59 -0300

    Create RHEmpresa.py

---
+++ RHEmpresa.py
@@ -0,0 +1,6 @@
+
+nome = input("Digite o nome do funcionario:")
+idade = input("Digite a idade do funcionario:")
+salario = float(input("Digite o salario desse funcionario:"))
+
+print("\nNome:", nome, "\nidade:", idade, "\nsalario:", salario)
