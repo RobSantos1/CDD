67674cb1cba9fa0a528ae20c7ed02a344191c1af
Robson Lázaro <97258787+RobsonLazaro@users.noreply.github.com>
2023-05-04 21:33:55 -0300

    Update categoriasNumeros.py

--- categoriasNumeros.py
+++ categoriasNumeros.py
@@ -1,3 +1,32 @@
 numeros = []
+pares = ""
+impares = ""
+positivo = ""
+negativo = ""
+zeros = ""
 for x in range(10):
     numeros.append(int(input("")))
+
+for y in range(10):
+    if numeros[y] % 2 == 0:
+        pares += f"[{y}] - {numeros[y]} é par\n"
+    if numeros[y] % 2 != 0:
+        impares += f"[{y}] - {numeros[y]} é impar\n"
+    if numeros[y] > 0:
+        positivo += f"[{y}] - {numeros[y]} é positivo\n"
+    if numeros[y] < 0:
+        negativo += f"[{y}] - {numeros[y]} é negativo\n"
+    else:
+        zeros += f"[{y}] - {numeros[y]} é zero!\n"
+
+print("============================================================================\n")
+print(pares)
+print("============================================================================\n")
+print(impares)
+print("============================================================================\n")
+print(positivo)
+print("============================================================================\n")
+print(negativo)
+print("============================================================================\n")
+print(zeros)
+print("============================================================================\n")
